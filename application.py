ed3ed7b17363cd7010335ec93dfa2225ffab822a
Mustafa Unal <94565017+MustafaxUK@users.noreply.github.com>
2022-03-05 09:57:42 +0000

    Set up database and created two data entries

--- application.py
+++ application.py
@@ -1,5 +1,21 @@
+from enum import unique
 from flask import Flask
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    phone_number = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.phone_number} - {self.name} - {self.email} "
+
+
+
 
 @app.route('/')
 def index():
